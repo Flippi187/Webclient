ebb65eb43bd73b915453ffc6e21cac9100aac9e9
Philipp de Zwart <philipp.de@bulme.at>
2022-01-21 17:54:54 +0000

    finished project

---
+++ webclient.h
@@ -0,0 +1,24 @@
+#ifndef WEBCLIENT_H
+#define WEBCLIENT_H
+
+#include "ui_webclient.h"
+#include <QAbstractSocket>
+#include <QTcpSocket>
+
+class Webclient : public QWidget, private Ui::Webclient
+{
+    Q_OBJECT
+
+public:
+    explicit Webclient(QWidget *parent = nullptr);
+private slots:
+    void on_button_clicked();
+    void tcp_connect();
+    void tcp_receive();
+private:
+    QTcpSocket* m_socket = nullptr;
+    const unsigned short m_port = 80;
+    QByteArray m_hostname;
+};
+
+#endif // WEBCLIENT_H
